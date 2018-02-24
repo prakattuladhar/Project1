82484b3f2a1abe6ef9f238557de8391d474cc18b
cquinn540 <cquinn540@gmail.com>
2018-02-24 13:34:11 -0600

    Adapted Client list to be serializable, iterable, and adapt a TreeMap will update remove function to look at shows associated with a client later

--- src/com/Project1/classes/ClientList.java
+++ src/com/Project1/classes/ClientList.java
@@ -1,17 +1,32 @@
 package com.Project1.classes;
 
-import java.util.ArrayList;
-
-public class ClientList {
-    private ArrayList<Client> list=new ArrayList<Client>();
-
-    public void append(Client client){
-        list.add(client);
+import java.util.*;
+import java.io.*;
+/**
+ * 
+ * 
+ * @author Colin Quinn
+ *
+ */
+public class ClientList implements Iterable<Client>, Serializable {
+	private static final long serialVersionUID = 1L;
+    private TreeMap<Integer, Client> map = new TreeMap<Integer, Client>();
+    /**
+     * 
+     * @param client
+     */
+    public void add(Client client){
+        map.put(client.getId(), client);
     }
+    /**
+     * client's id
+     * @param id
+     */
     public void remove(int id){
-
+    	map.remove(id);
     }
-    public ArrayList<Client> getList() {
-        return list;
+    
+    public Iterator<Client> iterator() {
+    	return map.values().iterator();
     }
 }
